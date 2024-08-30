74f0f5619e3431f140f3282328df09223c63f7b6
zoob <zubovalexey2001@gmail.com>
2024-08-30 23:45:52 +0300

    implement topic block component

--- src/components/TopicBlock.vue
+++ src/components/TopicBlock.vue
@@ -1,8 +1,20 @@
 <script setup lang="ts">
-
+const props = defineProps<{
+  header: string,
+  content: string[],
+}>();
 </script>
 
 <template>
+  <h1>{{ props.header }}</h1>
+  <ol>
+    <li
+      v-for="(element, index) in props.content"
+      :key="index"
+    >
+      {{ element }}
+    </li>
+  </ol>
 </template>
 
 <style scoped lang="scss">
